9ad6cf774278ecc70f3ca11dc76ff1380daccc4d
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2024-12-09 22:00:15 +0700

    update

--- src/components/MovieApp.vue
+++ src/components/MovieApp.vue
@@ -49,59 +49,68 @@ watch(search, (newSearch, oldSearch) => {
 const onError = (event: any) => {
   event.target.src = "https://placehold.co/60x89?text=Not Found";
 };
+
+const resetSearch = () => {
+  search.value = "";
+};
 </script>
 
 <template>
   <div class="flex items-center justify-center min-h-screen">
     <div class="w-1/2">
-      <div class="flex items-center justify-center">
+      <div class="flex items-center justify-center group">
         <input
           v-model="search"
-          class="p-4 w-11/12 rounded-l-2xl bg-slate-300 hover:bg-slate-200"
+          class="p-2 bg-slate-300 group-hover:bg-slate-200 w-full rounded-l-xl"
+          placeholder="Search for a movie..."
         />
+        <div v-if="search">
+          <button class="p-2 text-white bg-slate-300" @click="resetSearch">
+            ❌
+          </button>
+        </div>
         <button
-          class="p-4 w-1/12 rounded-r-2xl text-white bg-blue-500 hover:bg-blue-600"
+          class="p-2 text-white bg-slate-300 rounded-r-xl group-hover:bg-slate-200"
         >
           🔍
         </button>
       </div>
-      <div class="max-h-[60vh] overflow-y-auto">
-        <div class="px-6 bg-yellow-50 rounded-2xl mt-4">
-          <div v-if="loading">loading...</div>
-          <div v-else>
-            <div v-if="movies">
-              <div
-                v-for="movie in movies"
-                :key="movie.imdbID"
-                class="py-6 [&:not(:last-child)]:border-b border-red-200"
-              >
-                <div class="flex gap-4">
-                  <figure class="w-[60px] h-[89px]">
-                    <img
-                      width="60"
-                      height="89"
-                      :src="movie.Poster"
-                      :alt="`${movie.Title} Poster`"
-                      class="object-cover w-full h-full rounded-xl"
-                      @error="onError"
-                    />
-                  </figure>
-                  <div>
-                    <h3 class="line-clamp-1">{{ movie.Title }}</h3>
-                    <p>{{ movie.Year }}</p>
-                    <div class="flex gap-2 items-center">
-                      <div v-if="movie.Type === 'movie'">🎥</div>
-                      <div v-else>📺</div>
-                    </div>
+      <div
+        class="px-6 max-h-[60vh] bg-yellow-50 rounded-2xl mt-4 overflow-y-auto"
+      >
+        <div v-if="loading" class="py-4 rounded-2xl">loading...</div>
+        <div v-else>
+          <div v-if="movies">
+            <div
+              v-for="movie in movies"
+              :key="movie.imdbID"
+              class="py-6 [&:not(:last-child)]:border-b border-red-200"
+            >
+              <div class="flex gap-4">
+                <figure class="w-[60px] h-[89px]">
+                  <img
+                    width="60"
+                    height="89"
+                    :src="movie.Poster"
+                    :alt="`${movie.Title} Poster`"
+                    class="object-cover w-full h-full rounded-xl"
+                    @error="onError"
+                  />
+                </figure>
+                <div>
+                  <h3 class="line-clamp-1">{{ movie.Title }}</h3>
+                  <p>{{ movie.Year }}</p>
+                  <div class="flex gap-2 items-center">
+                    <div v-if="movie.Type === 'movie'">🎥</div>
+                    <div v-else>📺</div>
                   </div>
                 </div>
               </div>
             </div>
-            <div v-else>Error</div>
           </div>
         </div>
       </div>
-      <button
+      <!-- <button
         @click="store.count++"
         class="py-2 px-4 bg-neutral-100 hover:bg-neutral-200 rounded-xl"
       >
@@ -113,7 +122,7 @@ const onError = (event: any) => {
       >
         Dec
       </button>
-      <input v-model="store.query" class="border p-2 rounded-xl" />
+      <input v-model="store.query" class="border p-2 rounded-xl" /> -->
     </div>
   </div>
 </template>
